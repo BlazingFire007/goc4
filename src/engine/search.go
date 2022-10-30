6b4069158b4ac4f0bb3a4c73d34ac2759b181d1f
BlazingFire007 <elijahrich02@icloud.com>
2022-10-30 01:02:06 -0500

    bug fixes

--- src/engine/search.go
+++ src/engine/search.go
@@ -14,7 +14,7 @@ var nodes uint64 = 0
 
 func Root(b board.Board, seconds float64) byte {
 	const maxDepth = 43
-	// var bestScore int = -1000
+	var bestScore int = -1000
 	var bestMove byte
 	start := time.Now()
 	for depth := 11; depth <= maxDepth; depth++ {
@@ -23,8 +23,10 @@ func Root(b board.Board, seconds float64) byte {
 		}
 		move, score := RootSearch(b, depth, start, seconds)
 		fmt.Printf("Depth: %d, Move: %s, Score: %d\n", depth, string(move), score)
-		// bestScore = score
-		bestMove = move
+		if score > bestScore {
+			bestScore = score
+			bestMove = move
+		}
 	}
 	fmt.Println("Nodes: ", nodes)
 	fmt.Printf("Time: %fs\n", math.Round(time.Since(start).Seconds()*100)/100)
@@ -86,23 +88,23 @@ func negamax(b board.Board, depth, alpha, beta, ply int) int {
 
 	var bestScore int = -1000
 	moves := board.GetMoves(b)
+	if len(moves) == 0 {
+		return 0
+	}
 	var score int
 	for _, move := range moves {
 		b.Move(move)
 		key := cache.Key(cache.ZobristHash(b))
 		val, exists := table.Get(key)
 		if exists && val.Depth >= depth {
-			score = -val.Score
+			score = val.Score
 		} else {
 			score = -negamax(b, depth-1, -beta, -alpha, ply+1)
-			table.Set(key, cache.Value{Depth: depth, Score: -score})
+			table.Set(key, cache.Value{Depth: depth, Score: score})
 		}
 		b.Undo(move)
 		if score > bestScore {
 			bestScore = score
-		}
-		if score > 47 {
-			return score
 		}
 		if bestScore > alpha {
 			alpha = bestScore
